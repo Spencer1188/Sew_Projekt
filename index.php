35cf673ab6a5e265936d8f21a5b3ae32d9207c22
Maximilian.goeckler <maximilian.goeckler@sz-ybbs.ac.at>
2019-03-21 13:12:26 +0100

    First

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Einkaufsliste</title>
+	<link type="text/css" rel="stylesheet" href="asserts/css/bootstrap.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">Logo</a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+</nav>
+	
+	<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+	
+	<script src="asserts/js/bootstrap.js"></script>
+	<script src="asserts/js/jquery.js"></script>
+	<script src="asserts/js/chart.js"></script>
+</body>
+</html>
