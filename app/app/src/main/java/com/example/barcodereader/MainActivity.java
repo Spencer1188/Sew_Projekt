ba6b91a3ff4f52250b0aea7dae8980025ea91220
Pheeerox <mathias.fally2@gmail.com>
2019-04-06 19:38:32 +0200

    LoginRequest OKHTTP

--- app/app/src/main/java/com/example/barcodereader/MainActivity.java
+++ app/app/src/main/java/com/example/barcodereader/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
@@ -20,14 +21,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     private Button scanBtn,reloaditems;
-    private TextView formatTxt, contentTxt;
+    private TextView formatTxt, contentTxt,responsetxt;
     private ListView List;
 
     @Override
@@ -66,18 +75,33 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
             formatTxt.setText("FORMAT: " + scanFormat);
             contentTxt.setText("CONTENT: " + scanContent);
+            try {
+                sendData(scanFormat,scanContent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-            sendAccelerationData(scanFormat,scanContent);
         }
         else{
             formatTxt.setText("Error");
         }
     }
 
-    private void sendAccelerationData(String id, String name)
-    {
-        Send_Scan_Data r = new Send_Scan_Data();
-        r.execute(id,name);
+    void sendData(String id, String name) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://mgoeckler.ddns.net/Sew_Projekt/web/api/getjson.php").newBuilder();
+        urlBuilder.addQueryParameter("func", "additem");
+        urlBuilder.addQueryParameter("prefix", id);
+        urlBuilder.addQueryParameter("eantoken", name);
+        urlBuilder.addQueryParameter("usrid", "1");
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
     }
 
 }
